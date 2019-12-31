62744fc24a2ef5b83a823c06b2d7b50fe2eb449c
Archaeologist03 <omniiknight@hotmail.com>
2019-12-31 02:43:19 +0100

    Procfile, Hittin root try

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', async (req, res) => {
+  res.send('Yo, its up.');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
