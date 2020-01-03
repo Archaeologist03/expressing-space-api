dfc0cc44fbe5e2ce94a8167162a264874a1376b2
Archaeologist03 <omniiknight@hotmail.com>
2020-01-03 23:42:34 +0100

    Adding Typescript - still figuring the way..

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 const express = require('express');
+import { Request, Response } from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', async (req, res) => {
-  res.send('Yo, its up.');
+const a = 22;
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send({ username: 'x' });
+
+  console.log(123);
 });
 
 app.listen(PORT, () => {
